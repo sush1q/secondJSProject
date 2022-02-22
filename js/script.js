ff4a57745b2a99bf4363d031024afeb14f42d00d
sush1 <pavelsirotkin@MacBook-Air.local>
2022-02-22 16:59:25 +0300

    com

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@
         }
     });
 
-    let deadline = '2022-02-17';
+    let deadline = '2022-03-27';
         
 
     function getTimeRemaining(endtime){
@@ -128,74 +128,62 @@
 
     statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function(event){
+
+function sendForm(elem){
+    elem.addEventListener('submit', function(event){
         event.preventDefault();
-        form.appendChild(statusMessage);
-
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //FormData Format
-        // request.setRequestHeader('Content-Type', 'application/json; charset=utf-8')   //JSON Format
-
-        let formData = new FormData(form);
-
-        // let obj = {};     //JSON Format
-        // formData.forEach(function(value, keyframes){
-        //     obj[key] = value;
-        // });                                //JSON Format
-        // let json = JSON.stringify(obj);    //JSON Format
-
-        request.send(formData);  //FormData Format
-        // request.send(json); //JSON Format
-
-
-        request.addEventListener('readystatechange', function(){
-            if(request.readyState < 4){
-                statusMessage.innerHTML = message.loading;
-            } else if(request.readyState == 4 && request.status == 200){
-                statusMessage.innerHTML = message.success;
-            } else{
-                statusMessage.innerHTML = message.failure;
-            }
-        });
-        for(let i = 0; i < input.length; i++){
-            input[i].value = '';
-        } 
-    });
-
-    contactForm.addEventListener('submit', function(event){
-        event.preventDefault();
-        contactForm.appendChild(statusMessage);
-
-        let request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //FormData Format
-        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8')   //JSON Format
-
-        let formData = new FormData(contactForm);
-
-        let obj = {};     //JSON Format
-        formData.forEach(function(value, keyframes){
+        elem.appendChild(statusMessage);
+        let formData = new FormData(elem);
+        console.log('formData ' + formData);
+        let obj = {};
+        formData.forEach(function(value, key){
             obj[key] = value;
-        });                                //JSON Format
-        let json = JSON.stringify(obj);    //JSON Format
+            console.log('obj ' + obj);
+        })
+        let json = JSON.stringify(obj);
+        console.log('json ' + json);
 
-        // request.send(formData);  //FormData Format
-        request.send(json); //JSON Format
 
+        function postData(data){
+            return new Promise(function(resolve, reject){
+                let request = new XMLHttpRequest();
 
-        request.addEventListener('readystatechange', function(){
-            if(request.readyState < 4){
-                statusMessage.innerHTML = message.loading;
-            } else if(request.readyState == 4 && request.status == 200){
-                statusMessage.innerHTML = message.success;
-            } else{
-                statusMessage.innerHTML = message.failure;
+                request.open('POST', 'server.php');
+
+                request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+
+                request.onreadystatechange =  function(){
+                    if(request.readyState < 4){
+                        resolve()
+                    } else if(request.readyState === 4){
+                        if (request.status == 200 && request.readyState < 3){
+                            resolve()
+                        }
+                        else{
+                            reject()
+                        }
+                    }
+                }
+                request.send(data);
+            });
+        }
+
+        function clearInput(){  
+            for(let i = 0; i < input.length; i++){
+                input[i].value = '';
             }
-        });
-    
-        for(let i = 0; i < input.length; i++){
-            input[i].value = '';
-        } 
+        }
+
+        postData(json)
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(()=> statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput)
     });
+}
+sendForm(form);
+sendForm(contactForm);
+
+
+// });
 // });
